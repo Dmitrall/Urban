634d1c514e81c975b4af4e309dbc19ba0314686f
Dmitrall <bannikov_d@mail.ru>
2024-09-05 19:01:17 +0500

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Dmitry'
+print('Name:',name,)
+age = 33
+print ('Age:',age,)
+new_age = age + 1
+print ('New age:' ,new_age,)
+is_student = 'Dmitry' != 'student'
+print ('Is_student:' ,is_student)
