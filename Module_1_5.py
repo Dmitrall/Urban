8ed82768de0fcac44279e17116f5a6cc1ad1b94e
Dmitrall <bannikov_d@mail.ru>
2024-09-11 13:42:35 +0500

    Add files via upload

---
+++ Module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var_= 1, 2, 3, 'hello', 'world', True
+print(immutable_var_)
+#immutable_var_[1] = 10
+#print(immutable_var_)
+
+mutable_list_= ([10, 20, 30] ,'good', 'day', False)
+print(mutable_list_)
+mutable_list_[0][1] = 200
+print(mutable_list_)
