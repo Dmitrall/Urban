2ee11d0e47a4a251b53d5f5f91b3367a0d15c5bf
Dmitrall <bannikov_d@mail.ru>
2024-10-04 15:48:28 +0500

    Add files via upload

--- module_3_5.py
+++ module_3_5.py
@@ -1,13 +1,16 @@
 def get_multiplied_digits (number):
     str_number = str(number)
-
-    if len(str_number) > 1:
-        first = int(str_number[0])
-        return first * get_multiplied_digits(int(str_number[1:]))
-
-    else:
-        return int(str_number)
+    res_str_number = str_number.strip("0")
+    first = str(int(res_str_number[0]))
+    res_first = int(first)
 
 
-result = get_multiplied_digits(40203)
-print(result)
+    if len(res_str_number) > 1:
+        return res_first * get_multiplied_digits(int(res_str_number[1:]))
+
+    elif len(res_str_number) <= 1:
+        return res_first
+
+print(get_multiplied_digits(40203))
+print(get_multiplied_digits(402030))
+print(get_multiplied_digits(0402030)) #по словам преподавателя ноль должен убираться, почему ошибка, я не могу понять. Консультации не смог добиться
