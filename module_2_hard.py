13e806357401e8f4b37878021b0dd2102487dcef
Dmitrall <bannikov_d@mail.ru>
2025-01-10 14:52:58 +0500

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -3,15 +3,13 @@ import random
 number = random.randint(3, 20)
 print(f"Случайное число: {number}")
 
-pairs = []
-
+pairs = ''
 
 for i in range(1, 21):
 
     for j in range(i + 1, 21):
         if number % (i + j) == 0:
-            pairs.append((i, j))
+            pairs = pairs + str(i) + str(j)
 
-print("Найденные пары:")
-for pair in pairs:
-    print(pair)
+print('Первое число:', number)
+print('Второе число для вставки:', pairs, ' - пароль')
